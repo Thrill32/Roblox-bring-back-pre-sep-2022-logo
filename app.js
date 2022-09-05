101414623d095cf9b3be76ae5b0e132a8c804e9a
Thrill32 <105758293+Thrill32@users.noreply.github.com>
2022-09-04 21:50:57 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-//https://1000logos.net/wp-content/uploads/2021/04/Roblox-logo.png
+//Thrill32#0001
 
 function init(){
-   
-   var iconlogoclass = document.getElementsByClassName('icon-logo')[0];
+    var iconlogoclass = document.getElementsByClassName('icon-logo')[0];
     if (iconlogoclass != null) {
-      
-        
         const style = window.getComputedStyle(iconlogoclass);
-        console.log(style["background-image"]);
-        
-        iconlogoclass.style.backgroundImage = "url(https://download851.mediafire.com/wwwqnwf6fyng/3vpq1noghlhq409/old+roblox+logo+1.png)"
-        
+        var Sstyle = JSON.stringify(style["background-image"]);
+        console.log(Sstyle); 
+        if (Sstyle.search("Light")) {
+            iconlogoclass.style.backgroundImage = "url(https://download1487.mediafire.com/7gsan07ip3tg/ga897nsw7lz8cuj/light+logo.png)"
+        } else if (Sstyle.search("Dark")) {
+            iconlogoclass.style.backgroundImage = "url(https://download1491.mediafire.com/04a7mtzg9zdg/ntvkajywkfp6d4u/dark+logo.png)"
+        } else {
+            console.log("thrill fatal error 2 :sob:")
+            iconlogoclass.style.backgroundImage = "url(https://download851.mediafire.com/wwwqnwf6fyng/3vpq1noghlhq409/old+roblox+logo+1.png)" //border logo 
+        };   
     } else {
-        console.log("thrill fatal error :sob:");
-    }
-    
+       console.log("thrill fatal error :sob:");
+    } 
 }
 window.onload = init();
 
